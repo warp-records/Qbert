adf63f188d1fc10b4acb869a6a074466f369ce45
Rift <no@e.mail>
2024-03-11 23:13:57 -0400

    Implement horizontal rotation

--- cube.cpp
+++ cube.cpp
@@ -3,13 +3,231 @@
 
 
 Cube::Cube() {
-	top =    WhiteSide;
-	bottom = GreenSide;
-	front =  BlueSide;
-	back =   OrangeSide;
-	left =   RedSide;
-	right =  YellowSide;
+	top =    WhiteFace;
+	bottom = GreenFace;
+	front =  BlueFace;
+	back =   OrangeFace;
+	left =   RedFace;
+	right =  YellowFace;
 }
 
-//Cube rotate(Direction dir, std::variant<Column, Row> line) {}
-//bool is_solved() {}
+uint32_t rotFaceLeft(uint32_t face);
+uint32_t rotFaceRight(uint32_t face);
+uint32_t rotFace180(uint32_t face);
+
+Cube Cube::rotHoriz(Row line, Direction dir) {
+
+	uint32_t maskType;
+	
+	switch (line) {
+		case (Row::Top) : {
+			outwardFace = &top;
+			maskType = Mask::Row::Top;
+			break;
+		}
+
+		case (Row::Middle): {
+			maskType = Mask::Row::Middle;
+			break;
+		}
+
+		case (Row::Bottom): {
+			outwardFace = &bottom;
+			maskType = Mask::Row::Bottom;
+			break;
+		}
+	}
+
+	if (line != Row::Middle) {
+		switch (dir) {
+			case (Direction::Left) : {
+				*outwardFace = rotFaceLeft(*outwardFace);
+				break;
+			}
+
+			case (Direction::Right) : {
+				*outwardFace = rotFaceRight(*outwardFace);
+				break;
+			}
+
+			case (Direction::_180) : {
+				*outwardFace = rotFace180(*outwardFace);
+				break;
+			}
+		}
+	}
+
+	uint16_t tmp = front&maskType;
+
+	switch (dir) {
+		case (Direction::Left) : {
+			front &= ~maskType;
+			front |= (right&maskType);
+
+			right &= ~maskType;
+			right |= (back&maskType);
+
+			back &= ~maskType;
+			back |= (left&maskType);
+
+			left &= ~maskType;
+			left |= tmp;
+			break;
+		}
+
+		case (Direction::Right) : {
+			front &= ~maskType;
+			front |= (left&maskType);
+
+			left &= ~maskType;
+			left |= (back&maskType);
+
+			back &= ~maskType;
+			back |= (right&maskType);
+
+			right &= ~maskType;
+			right |= tmp;
+			break;
+		}
+
+		case (Direction::_180) : {
+			front &= ~maskType;
+			front |= (back&maskType);
+
+			back &= ~maskType;
+			back |= tmp;
+
+			tmp = right&maskType;
+
+			right &= ~maskType;
+			right |= (left&maskType);
+
+			left &= ~maskType;
+			left |= tmp;
+			break;
+		}
+	}
+}
+
+/*
+Cube Cube::rotateVert(Column line, Direction dir) {
+
+	uint32_t maskType;
+	
+	switch (line) {
+		case (Row::Left) : {
+			outwardFace = &left;
+			maskType = Mask::Column::Left;
+			break;
+		}
+
+		case (Row::Middle): {
+			maskType = Mask::Column::Middle;
+			break;
+		}
+
+		case (Row::Bottom): {
+			outwardFace = &bottom;
+			maskType = Mask::Column::Right;
+			break;
+		}
+	}
+
+	if (line != Row::Middle) {
+		switch (dir) {
+			case (Direction::Up) : {
+				*outwardFace = rotFaceLeft(*outwardFace);
+				break;
+			}
+
+			case (Direction::Down) : {
+				*outwardFace = rotFaceRight(*outwardFace);
+				break;
+			}
+
+			case (Direction::_180) : {
+				*outwardFace = rotFace180(*outwardFace);
+				break;
+			}
+		}
+	}
+
+	uint16_t tmp = top&maskType;
+
+	top &= ~maskType;
+	top |= (right&maskType);
+
+	right &= ~maskType;
+	right |= (back&maskType);
+
+	back &= ~maskType;
+	back |= (left&maskType);
+
+	left &= ~maskType;
+	left |= tmp;
+}*/
+
+
+uint32_t Cube::rotFaceLeft(uint32_t face) {
+	uint32_t newFace = 0x0000;
+	//(0b111 << original position offset) (<< or >>) new offset by shift
+	//Group the same shift operations once you are sure this works
+	newFace |= (face & (0b111<<0*3)) << 6*3;
+	newFace |= (face & (0b111<<1*3)) << 2*3;
+	newFace |= (face & (0b111<<2*3)) >> 2*3;
+	newFace |= (face & (0b111<<3*3)) << 4*3;
+	newFace |= (face & (0b111<<5*3)) >> 4*3;
+	newFace |= (face & (0b111<<6*3)) << 2*3;
+	newFace |= (face & (0b111<<7*3)) >> 2*3;
+	newFace |= (face & (0b111<<8*3)) >> 6*3;
+
+	return newFace;
+}
+
+uint32_t Cube::rotFaceRight(uint32_t face) {
+	uint32_t newFace = 0x0000;
+
+	newFace |= (face & (0b111<<0*3)) << 2*3;
+	newFace |= (face & (0b111<<1*3)) << 4*3;
+	newFace |= (face & (0b111<<2*3)) << 6*3;
+	newFace |= (face & (0b111<<3*3)) << 2*3;
+	newFace |= (face & (0b111<<5*3)) << 2*3;
+	newFace |= (face & (0b111<<6*3)) << 6*3;
+	newFace |= (face & (0b111<<7*3)) << 4*3;
+	newFace |= (face & (0b111<<8*3)) >> 2*3;
+
+	return newFace;
+}
+
+uint32_t Cube::rotFace180(uint32_t face) {
+	uint32_t newFace = 0x0000;
+
+	newFace |= (face & (0b111<<0*3)) << 2*3;
+	newFace |= (face & (0b111<<1*3)) << 4*3;
+	newFace |= (face & (0b111<<2*3)) << 6*3;
+	newFace |= (face & (0b111<<3*3)) << 2*3;
+	newFace |= (face & (0b111<<5*3)) << 2*3;
+	newFace |= (face & (0b111<<6*3)) << 6*3;
+	newFace |= (face & (0b111<<7*3)) << 4*3;
+	newFace |= (face & (0b111<<8*3)) >> 2*3;
+
+	return newFace;
+}
+
+uint32_t Cube::rotFace180(uint32_t face) {
+	uint32_t newFace = 0x0000;
+
+	newFace |= (face & (0b111<<0*3)) << 8*3;
+	newFace |= (face & (0b111<<1*3)) << 6*3;
+	newFace |= (face & (0b111<<2*3)) << 4*3;
+	newFace |= (face & (0b111<<3*3)) << 2*3;
+	newFace |= (face & (0b111<<5*3)) >> 2*3;
+	newFace |= (face & (0b111<<6*3)) >> 4*3;
+	newFace |= (face & (0b111<<7*3)) << 6*3;
+	newFace |= (face & (0b111<<8*3)) >> 8*3;
+
+	return newFace;
+}
+
+
+//bool isSolved() {}
--- cube.hpp
+++ cube.hpp
@@ -1,8 +1,9 @@
+
 #include <cstdint>
 #include <variant>
 
 
-enum class Row {
+enum class Line {
 	Top, Middle, Bottom
 };
 
@@ -11,29 +12,45 @@ enum class Column {
 };
 
 enum class Direction {
-	Up, Down, Left, Right
-};
+	Up, Down, Left, Right, _180
+}
 
-enum class Color {
+namespace Mask {
+	enum class Column {
+		Left =   0x1C0E07 << 6*3,
+		Middle = 0x1C0E07 << 3*3,
+		Right =  0x1C0E07
+	}
+
+	enum class Row {
+		Top = 0xff << 18*3,
+		Middle = 0xff << 9*3,
+		Bottom = 0xff,
+	}
+}
+
+
+
+enum Color {
 	White =  0b000,
 	Green =  0b001,
 	Blue =   0b010,
 	Orange = 0b011,
 	Red = 	 0b100,
-	Yellow = 0b101,
+	Yellow = 0b101
 };
 
-enum WholeSide {
-	WhiteSide =  0x0000000,
-	GreenSide =  0x1249249,
-	BlueSide =   0x2492492,
-	OrangeSide = 0x36DB6DB,
-	RedSide = 	 0x4924924,
-	YellowSide = 0x5B6DB6D,
+enum SolvedFace {
+	WhiteFace =  0x0000000,
+	GreenFace =  0x1249249,
+	BlueFace =   0x2492492,
+	OrangeFace = 0x36DB6DB,
+	RedFace = 	 0x4924924,
+	YellowFace = 0x5B6DB6D
 }
 
-
 struct Cube {
+
 
 uint32_t top, bottom,
 		 front, back,
@@ -44,7 +61,17 @@ uint32_t top, bottom,
 					front, back,
 					left, right);
 
-	Cube rotate(Direction dir, std::variant<Column, Row> line);
-	bool is_solved();
+
+	Cube rotateHoriz(Row line, Direction dir);
+	Cube rotateVert(Column line, Direction dir);
+
+	bool isSolved();
+
+private:
+	//If this becomes a bottleneck, it could possibly
+	//be sped up with a permute instruction
+	uint32_t rotFaceLeft(uint32_t face);
+	uint32_t rotFaceRight(uint32_t face);
+	uint32_t rotFace180(uint32_t face);
 
 };
